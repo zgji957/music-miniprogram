aad0870f89bc04212cfd0467730fd26600dda391
sam_gzf <434901323@qq.com>
2019-10-05 14:38:08 +0800

    update

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -3,6 +3,10 @@ const cloud = require('wx-server-sdk')
 
 const TcbRouter = require('tcb-router')
 
+const rp = require('request-promise')
+
+const BASE_URL = 'http://musicapi.xiecheng.live'
+
 cloud.init()
 
 // 云函数入口函数
@@ -15,6 +19,13 @@ exports.main = async (event, context) => {
       .limit(event.count)
       .orderBy('createTime', 'desc')
       .get()
+  })
+
+  app.router('musiclist',async(ctx,next)=>{
+    ctx.body = await rp(BASE_URL+'/playlist/detail?id='+event.playlistId)
+    .then((res)=>{
+      return JSON.parse(res)
+    })
   })
 
   return app.serve()
--- miniprogram/pages/musiclist/musiclist.js
+++ miniprogram/pages/musiclist/musiclist.js
@@ -5,14 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    musiclist:[],
+    listInfo:{},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('options:',options)
+    wx.cloud.callFunction({
+      name:'music',
+      data:{
+        playlistId:options.playlistId,
+        $url:'musiclist'
+      }
+    }).then((res)=>{
+      console.log('res',res)
+      this.setData({
+        musiclist: res.result.playlist.tracks,
+        listInfo:{
+          coverImgUrl: res.result.playlist.coverImgUrl,
+          name: res.result.playlist.name,
+        }
+      })
+    })
   },
 
   /**
